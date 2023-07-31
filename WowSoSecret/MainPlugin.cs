5dfab7d6db9de2eaa3fa97c325c5b3d28c06e32c
Raoul1808 <raoulthegeek@gmail.com>
2023-07-31 17:47:20 +0200

    Remove debug log, bump up version

--- WowSoSecret/MainPlugin.cs
+++ WowSoSecret/MainPlugin.cs
@@ -10,14 +10,13 @@ namespace WowSoSecret
     {
         private const string Guid = "srxd.raoul1808.wowsosecret";
         private const string Name = "Wow So Secret";
-        private const string Version = "0.1.0";
+        private const string Version = "1.0.0";
 
         private static ManualLogSource _logger;
         
         void Awake()
         {
             _logger = Logger;
-            Log("Wow So Secret");
             Harmony harmony = new Harmony(Guid);
             harmony.PatchAll(typeof(PresencePatches));
             harmony.PatchAll(Assembly.GetExecutingAssembly());  // For some completely utterly unknown f**ing reason, this is required to apply the inner PresencePatch patch class.

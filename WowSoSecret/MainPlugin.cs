7f43194652a541b35025b3395de65d57b0f5baaf
Raoul1808 <raoulthegeek@gmail.com>
2024-09-21 19:55:37 +0200

    Bump up version

--- WowSoSecret/MainPlugin.cs
+++ WowSoSecret/MainPlugin.cs
@@ -11,23 +11,23 @@ namespace WowSoSecret
     {
         private const string Guid = "srxd.raoul1808.wowsosecret";
         private const string Name = "Wow So Secret";
-        private const string Version = "1.1.0";
-        
+        private const string Version = "2.0.0";
+
         private const string SpinCoreGuid = "srxd.raoul1808.spincore";
 
         private static ManualLogSource _logger;
 
         private static string _secretsPath = Path.Combine(Paths.ConfigPath, "SecretTexts.json");
-        
+
         void Awake()
         {
             _logger = Logger;
-            
+
             if (!File.Exists(_secretsPath))
                 File.WriteAllText(_secretsPath, SecretTexts.Default().ToJson());
 
             SecretManager.Init(Config);
-            
+
             Harmony harmony = new Harmony(Guid);
             harmony.PatchAll(typeof(PresencePatches));
             SpinCoreSupport.Init();

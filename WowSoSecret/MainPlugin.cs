1c111430f753b7baa3c7dc8a650b474a01266298
Raoul1808 <raoulthegeek@gmail.com>
2025-10-20 19:24:33 +0200

    Bump up mod version

--- WowSoSecret/MainPlugin.cs
+++ WowSoSecret/MainPlugin.cs
@@ -10,7 +10,7 @@ namespace WowSoSecret
     {
         private const string Guid = "srxd.raoul1808.wowsosecret";
         private const string Name = "Wow So Secret";
-        private const string Version = "2.1.0";
+        private const string Version = "2.1.1";
 
         private const string SpinCoreGuid = "srxd.raoul1808.spincore";
 
